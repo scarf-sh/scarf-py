f40d5aaccd11b0e451932a7f0e5849da2a15d92f
aviaviavi <mail@avi.press>
2024-12-29 13:36:41 -0500

    cleanup

--- setup.py
+++ setup.py
@@ -13,18 +13,6 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/scarf-sh/scarf-py",
     packages=find_packages(),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-    ],
     python_requires=">=3.7",
     install_requires=[
         "requests>=2.25.0",
